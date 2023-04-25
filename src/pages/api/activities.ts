b1e90ed4a22ad4440776c10bc19ed439f98ba025
aditipolkam <aditipolkam@gmail.com>
2023-04-25 23:09:22 +0530

    feat: :globe_with_meridians: geolocation based sorting of activities

--- src/pages/api/activities.ts
+++ src/pages/api/activities.ts
@@ -1,6 +1,49 @@
 import { NextApiRequest, NextApiResponse } from "next"
 import { admin, db} from "@/config/firebaseAdmin";
 import { ACTIVITIES_COLLECTION } from "@/config/dbVars";
+import nearbySort from "nearby-sort";
+import haversine from 'haversine';
+import { Activity } from "@/types";
+
+function sortOnDistance(radius:any, location:any,mockData:Activity[]){
+  const r = Number(radius);
+  const currentLocation = {
+    "lat": Number(location.lat),
+    "long": Number(location.lng)
+  };
+  // { lat: 18.6161, lng: 73.7286 }
+  const docs = nearbySort(currentLocation, mockData.map((v) => {
+      return {
+          ...v,
+          lat: v.location.coordinates.lat,
+          long: v.location.coordinates.lng
+      }
+  }), true).then((res: (Activity & { lat: number, long: number })[]) => {
+      console.dir(res, { depth: null, colors: true });
+      return res;
+  }).then((res) => {
+      // filter res by a minimum distance
+      return res.filter((v) => {
+          return haversine({
+              latitude: currentLocation.lat,
+              longitude: currentLocation.long
+          }, {
+              latitude: v.lat,
+              longitude: v.long
+          }, {
+              threshold: r * 1000, // 20 km
+              unit: 'meter'
+          })
+      })
+  }).then((res) => {
+      // console.log("filtered")
+      // console.dir(res, { depth: null, colors: true })
+      return res;
+      
+  });
+  return docs;
+}
+
 
 export default async function handler(req:NextApiRequest, res:NextApiResponse) {
   try{
@@ -13,7 +56,9 @@ export default async function handler(req:NextApiRequest, res:NextApiResponse) {
     if(!snapshot) res.status(404).json({message:"Nothing found"})
     const documents =  snapshot.docs.map(doc => doc.data());
     if(documents.length == 0) res.status(404).json({message:"Nothing found"})
-    res.status(200).json(documents)
+    const sortedDocs = await sortOnDistance(radius,location.coordinates,documents as Activity[])
+    console.log("sortedDocs",sortedDocs)
+    res.status(200).json(sortedDocs)
   }
   catch(error:any){
     res.status(401).json({ message: error.message  })
--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -6,7 +6,7 @@ import { useContext } from "react";
 import { useRouter } from "next/router";
 import axios from "axios";
 import { Activity } from "@/types";
-import { SimpleGrid } from "@chakra-ui/react";
+import { SimpleGrid, Input, FormControl, FormLabel } from "@chakra-ui/react";
 import ActivityCard from "@/components/ActivityCard";
 import CustomButton from "@/components/common/CustomButton";
 import Link from "next/link";
@@ -16,16 +16,17 @@ const Index = () => {
   const location = useGeoLocation();
   const { user } = useContext(AuthContext);
   const [activities, setActivities] = React.useState<Activity[]>([]);
-  console.log(location);
+  const [radius, setRadius] = React.useState<string>("20");
 
   const getActivities = async () => {
     try {
       const res = await axios.post(`/api/activities`, {
         location,
-        radius: 100,
+        radius,
       });
       if (res.status == 400) return console.log(res.data);
       if (res.status !== 200) return console.log(res.data);
+      console.log(res);
       setActivities(res.data);
     } catch (err) {
       console.log(err);
@@ -36,14 +37,25 @@ const Index = () => {
     if (!user) {
       router.push("/");
     }
-    if (location.loaded) {
-      getActivities();
+    if (!location.loaded) return console.log("location not loaded");
+    if (location.coordinates) {
+      const { lat, lng } = location.coordinates;
+      if (lat && lng) getActivities();
     }
-  }, [location.loaded]);
+  }, [location.loaded, location.coordinates, user, router, radius]);
   return (
-    <div>
+    <div className="mb-10">
       <div className="flex justify-between">
-        <div></div>
+        <div>
+          <FormControl>
+            <FormLabel>Radius</FormLabel>
+            <Input
+              placeholder="Radius"
+              value={radius}
+              onChange={(e) => setRadius(e.target.value)}
+            />
+          </FormControl>
+        </div>
         <Link href={"/signup/activities"}>
           <CustomButton variant="solid">Add Activity</CustomButton>
         </Link>
